5594f5a76c7f3727d3805a56484f122e2f611bad
Abhishek EH <collegewap@gmail.com>
2020-09-08 14:22:58 +0530

    Basic routing

--- src/App.js
+++ src/App.js
@@ -4,22 +4,38 @@ import { Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> |
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
       </nav>
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
 
 export const Home = () => {
-  return <div>Home</div>;
+  return <div>You are in Home page</div>;
 };
 export const About = () => {
-  return <div>About</div>;
+  return <div>This is the page where you put details about yourself</div>;
+};
+export const NotFound = () => {
+  return <div>This is a 404 page</div>;
 };
 
 export default App;
